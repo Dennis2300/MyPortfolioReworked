059179c70687f7d4802f5692454f165ab986dc30
Densanity <denniszixiang@gmail.com>
2025-08-14 20:44:07 +0200

    Enhance home page title with gradient text and new font
    Applied a gradient effect to the main title using a new .gradient-text class and increased its font size. Imported the Montserrat font from Google Fonts and updated styling for improved appearance and readability.

--- src/pages/home-page.vue
+++ src/pages/home-page.vue
@@ -2,7 +2,7 @@
   <div class="home-page-container">
     <div class="home-page-hero-container">
       <div class="home-page-title-container">
-        <h1>
+        <h1 class="gradient-text">
           Hello! <br />
           I'm Dennis-Zixiang Zhou
         </h1>
@@ -28,6 +28,8 @@
 <script setup></script>
 
 <style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap');
+
 .home-page-container {
   min-height: 100vh;
   margin: 0%;
@@ -46,15 +48,27 @@
   color: white;
   width: 50%;
   margin-left: 5%;
+  font-family: 'Montserrat', sans-serif;
+  font-weight: 700;
 }
 
 .home-page-title-container h1 {
-  font-size: 3rem;
+  font-size: 4rem;
   margin-bottom: 1rem;
 }
 
 .home-page-description {
   font-size: 1.2rem;
   line-height: 1.5;
+  color: lightgray;
+}
+
+.gradient-text {
+  background: linear-gradient(225deg, #0bb0bc 0%, #057e91 30%, #c01024 70%, #581521 100%);
+  -webkit-background-clip: text;
+  background-clip: text;
+  color: transparent;
+  font-weight: bold;
+  display: inline-block;
 }
 </style>
