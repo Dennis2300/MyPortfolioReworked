cc56ceafaeb5bd24015c190bfd0e5621aa632e9d
Densanity <denniszixiang@gmail.com>
2025-08-15 17:16:59 +0200

    Add footer and navbar components
    Introduced footerComponent.vue with social links and copyright, and navbarComponent.vue with navigation links to Home and Gallery. These components will help structure the application's layout.

---
+++ src/components/footerComponent.vue
@@ -0,0 +1,15 @@
+<template>
+  <footer>
+    <div class="footer-container">
+      <div class="footer-content">
+        <p>&copy; 2023 My Portfolio. All rights reserved.</p>
+        <p>Follow me on:</p>
+        <ul class="social-links">
+          <li><a href="https://github.com/yourusername" target="_blank">GitHub</a></li>
+          <li><a href="https://www.linkedin.com/in/yourusername" target="_blank">LinkedIn</a></li>
+          <li><a href="https://twitter.com/yourusername" target="_blank">Twitter</a></li>
+        </ul>
+      </div>
+    </div>
+  </footer>
+</template>
